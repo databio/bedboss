3cdf8e3be324df0c4ee21b4277a8d058d80d71f2
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-02-16 13:26:14 -0500

    for bed type catch all return bed type instead of "unknown_bedtype"

--- bedboss/bedclassifier/bedclassifier.py
+++ bedboss/bedclassifier/bedclassifier.py
@@ -235,7 +235,7 @@ def get_bed_type(bed: str, no_fail: Optional[bool] = True) -> Tuple[str, str]:
                     return f"bed{bedtype}+{n}", bed_type_named
 
         # This is to catch any files that are assigned a bed number but don't adhere to the above conditions
-        return f"bed{bedtype}+0", "unknown_bedtype"
+        return f"bed{bedtype}+0", bed_type_named
 
     else:
         return "unknown_bedtype", "unknown_bedtype"
