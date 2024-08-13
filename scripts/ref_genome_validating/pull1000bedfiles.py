e2c6502e24e62aff9847a82df6be64550fd215b2
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2024-08-13 11:11:15 -0400

    initial commit for pulling and caching 1000 bedfiles

---
+++ scripts/ref_genome_validating/pull1000bedfiles.py
@@ -0,0 +1,85 @@
+import gzip
+import argparse
+import logging
+import os
+import shutil
+
+import pipestat
+import pypiper
+from typing import Optional
+
+from bedboss.bedclassifier import get_bed_type
+from bedboss.exceptions import BedTypeException
+from geofetch import Finder, Geofetcher
+
+_LOGGER = logging.getLogger("bedboss")
+
+
+MAX_BEDFILES = 500
+RETRIEVE_BED_FILES = False  # get newer bedfiles?
+
+
+def main():
+    """
+    1. Pull ~ 1000 files for 3 different species for testing.
+
+    2. Cache the bed files for excluded ranges work.
+
+    """
+
+    print("Hello World")
+    # Make sure to have the IDE ignore these folders!!!!
+
+    data_output_path = os.path.abspath("data")
+    results_path = os.path.abspath("results")
+    logs_dir = os.path.join(results_path, "logs")
+
+    # print(data_output_path,results_path, logs_dir)
+
+    # Homo Sapiens
+
+    # filter = "\.(bed|narrowPeak|broadPeak)\."  #  regex filter
+    human_filter = "((bed) OR narrow peak) AND Homo sapiens[Organism]"
+    human_data_path = os.path.join(data_output_path, "homosapiens")
+    pull_1000_bedfiles(filter=human_filter, data_output_path=human_data_path)
+
+    pass
+
+
+def pull_1000_bedfiles(filter, data_output_path):
+    # make directory if it does not exist
+    try:
+        os.makedirs(data_output_path, exist_ok=True)
+    except OSError:
+        print(f"Directory already exists, skipping...")
+
+    print(filter)
+    print(data_output_path)
+
+    # Generate bed file list
+    gse_obj = Finder(filters=filter, retmax=MAX_BEDFILES)
+    gse_list = gse_obj.get_gse_by_date(
+        start_date="2020/08/01", end_date="2024/08/01"
+    )  #  1095 is three years worth
+    text_file_path = os.path.join(data_output_path, "bedfileslist.txt")
+    gse_obj.generate_file(text_file_path)
+
+    geofetcher_obj = Geofetcher(
+        filter_size="25MB",
+        data_source="samples",
+        geo_folder=data_output_path,
+        metadata_folder=data_output_path,
+        processed=True,
+        max_soft_size="20MB",
+        discard_soft=True,
+    )
+
+    geofetched = geofetcher_obj.get_projects(
+        input=os.path.join(data_output_path, "bedfileslist.txt"), just_metadata=False
+    )
+
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
