08f6427c609dcaf7a1b6291a4653ce02873a4753
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-01-23 18:20:51 -0500

    add notes and specific counts

--- scripts/bedclassifier_tuning/stats.py
+++ scripts/bedclassifier_tuning/stats.py
@@ -1,5 +1,20 @@
 # this is a script to run and data pulled directly from bedbase
 # it assumes you've pulled the data into a local csv for analysis
+
+# Three Required Fields, 9 additional for BED files
+# broadPeak = 6+3
+# narrowpeak = 6+4
+
+# We are classifying everything as bed if it is not broadPeak, narrowPeak
+# need file name, just so we can get extension, and the input_type
+
+# Sources ENCODE, GEO, only geo is flagging as success or fail
+
+# geo 1042 processed, 39 failed -> 3.7% for GEO
+
+# we can map genome to genome provided
+
+
 import os.path
 
 import matplotlib.colors as mcolors
@@ -40,6 +55,8 @@ total_rows = df_bed_types.shape[0]
 
 print(bed_format_counts)
 print(total_rows)
+
+# BE CAREFUL, THE POSITIONS MAY SHIFT
 narrowpeakcount = bed_format_counts.iloc[0]
 bedcount = bed_format_counts.iloc[1]
 broadpeakcount = bed_format_counts.iloc[2]
@@ -55,3 +72,23 @@ df_bed_types["bed_type"].value_counts(normalize=True).plot(kind="barh" )
 # pt.show()
 plt.savefig("./results/bed_type_distribution.png")
 plt.close()
+
+# Get 6+4 beds aka narrowPeaks
+bed_6plus4_rows = df_bed_types[df_bed_types['bed_type'] == 'bed6+4']
+bed_6plus4_rows_count = len(bed_6plus4_rows)
+print(f"6+4 vs # narrowpeaks: {bed_6plus4_rows_count},{narrowpeakcount}  {(narrowpeakcount/bed_6plus4_rows_count)*100}")
+
+# Get 6+3 beds
+bed_6plus3_rows = df_bed_types[df_bed_types['bed_type'] == 'bed6+3']
+bed_6plus3_rows_count = len(bed_6plus3_rows)
+print(f"6+3 vs # broadpeaks: {bed_6plus3_rows_count},{broadpeakcount}  {(broadpeakcount/bed_6plus3_rows_count)*100}")
+
+# Get all 3+# BEDS
+all_bed3=["bed3+0", "bed3+3","bed3+8", "bed3+2", "bed3+4", "bed3+1", "bed3+5","bed3+9"]
+bed3_rows = df_bed_types[df_bed_types['bed_type'].isin(all_bed3)]
+bed3_rows_count = len(bed3_rows)
+
+print(f"All other bed3+#: {bed3_rows_count}")
+
+print(f"Remaining: {total_rows-bed3_rows_count-bed_6plus3_rows_count-bed_6plus4_rows_count} , {(total_rows-bed3_rows_count-bed_6plus3_rows_count-bed_6plus4_rows_count)*100/total_rows}  ")
+
