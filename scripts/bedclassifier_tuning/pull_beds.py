3b0788e40e58d9649f08bfe777881e6704dcc164
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-01-30 13:53:59 -0500

    pull some broadpeak data

--- scripts/bedclassifier_tuning/pull_beds.py
+++ scripts/bedclassifier_tuning/pull_beds.py
@@ -16,18 +16,23 @@ df_bed_types = pd.read_csv(bed_types_path)
 
 # Get files that were not labeled at broadPeaks but do have the type as bed6+3
 not_broadpeak_digests = df_bed_types[(df_bed_types['bed_format'] == 'bed') & (df_bed_types['bed_type'] == 'bed6+3')]['id']
+# Get files that were labeled as broadPeak
+broadpeak_digests = df_bed_types[(df_bed_types['bed_format'] == 'broadpeak') & (df_bed_types['bed_type'] == 'bed6+3')]['id']
 
 dest_folder ="/home/drc/GITHUB/bedboss/bedboss/scripts/bedclassifier_tuning/data/notbrdpks"
+dest_folder_2 ="/home/drc/GITHUB/bedboss/bedboss/scripts/bedclassifier_tuning/data/brdpks"
 
 #print(not_broadpeak_digests.shape)
+count_nt_brdpeak = 0
+count_brdpeak = 0
 
-for digest in not_broadpeak_digests:
+for digest in broadpeak_digests[400:800]:
     #https://data2.bedbase.org/files/2/3/233479aab145cffe46221475d5af5fae.bed.gz
     #print(digest)
     # print(digest[0])
     url = f"https://data2.bedbase.org/files/{digest[0]}/{digest[1]}/{digest}.bed.gz"
     filename = url.split('/')[-1]  # Extract filename from URL
-    file_path = os.path.join(dest_folder, filename)
+    file_path = os.path.join(dest_folder_2, filename)
     #print(file_path)
     if not os.path.exists(file_path):
         try:
@@ -45,14 +50,19 @@ for digest in not_broadpeak_digests:
         except requests.exceptions.RequestException as e:
             print(f"Error downloading {url}: {e}")
     else:
-        #print(f"skipping file exists: {file_path}")
+        print(f"skipping file exists: {file_path}")
         pass
 
     result = get_bed_type(file_path)
     ##print(result)
+
     if result[1] != 'broadpeak':
         print(f"This one is not classified as broadpeak: {file_path}")
+        count_nt_brdpeak += 1
     else:
         print("FOUND BROADPEAK")
+        count_brdpeak +=1
 
+print("Originally classified as broadpeak. However, running classification without file-names")
+print(f"broadPeaks: {count_brdpeak} \nnot broadpeak:{count_nt_brdpeak}")
 #print(get_bed_type("/home/drc/test/957d1614e76987bb9d9f3a4f75d8ae50.bed.gz"))
